ea2911bcc9e0dd96a844682236242682dc15c940
Steph Samson <sdvsamson@gmail.com>
2014-09-24 16:46:33 -0400

    get args from url

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import BaseHTTPServer
 import os
 import time
+import re
+
 
 HOST_NAME = 'localhost'
 PORT_NUM = 4242
@@ -13,11 +15,20 @@ def bar():
   return 'bar'
 
 
+def hello(to):
+  return 'hello {}'.format(to)
+
+
+
 routes = {
   '/': hello_world,
   '/index': hello_world,
+  '/hello/:to': hello,
   '/bar': bar
 }
+
+pattern = re.compile('(?:\:)(\w+)+')
+
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_HEAD(self):
@@ -28,7 +39,16 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_GET(self):
     self.file_path = os.getcwd() + '/index.html'
     self.do_HEAD()
-    self.wfile.write(routes[self.path]())
+
+    path_args = pattern.findall(self.path)
+
+    self.wfile.write(path_args)
+
+    if self.path in routes:
+      self.wfile.write(routes[self.path]())
+    else:
+      self.send_error(404)
+
 
 if __name__ == '__main__':
   server_class = BaseHTTPServer.HTTPServer
