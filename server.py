a9ee8efa5781e162b0c7020b2194e22e3e8c8842
Steph Samson <sdvsamson@gmail.com>
2014-09-24 18:21:45 -0400

    POST from pymotw

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import os
 import time
 import argparse
 import re
+import cgi
 from routes import hello, hello_world
 
 
@@ -43,6 +44,38 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
       return
 
     self.send_error(404)
+
+  def do_POST(self):
+    form = cgi.FieldStorage(
+            fp=self.rfile, 
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+
+    # Begin the response
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write('Client: %s\n' % str(self.client_address))
+    self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
+    self.wfile.write('Path: %s\n' % self.path)
+    self.wfile.write('Form data:\n')
+
+    # Echo back information about what was posted in the form
+    for field in form.keys():
+        field_item = form[field]
+        if field_item.filename:
+            # The field contains an uploaded file
+            file_data = field_item.file.read()
+            file_len = len(file_data)
+            del file_data
+            self.wfile.write('\tUploaded %s as "%s" (%d bytes)\n' % \
+                    (field, field_item.filename, file_len))
+        else:
+            # Regular form value
+            self.wfile.write('\t%s=%s\n' % (field, form[field].value))
+    return
+
 
 
 
